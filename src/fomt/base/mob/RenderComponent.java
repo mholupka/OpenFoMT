45b1a9e88b635feac41299c78173fd2a47c7e71b
Mike Holupka <mholupka@verizon.net>
2014-11-01 18:45:48 -0400

    color change boys

--- src/fomt/base/mob/RenderComponent.java
+++ src/fomt/base/mob/RenderComponent.java
@@ -13,6 +13,15 @@ public class RenderComponent implements IRenderComponent {
 		lx += f * m.vx;
 		ly += f * m.vy;
 		
+		if (m.facing == Direction.DOWN)
+			GLRenderer.setColor(0, 0, 0, 1);
+		if (m.facing == Direction.UP)
+			GLRenderer.setColor(255, 0, 0, 1);
+		if (m.facing == Direction.LEFT)
+			GLRenderer.setColor(0, 255, 0, 1);
+		if (m.facing == Direction.RIGHT)
+			GLRenderer.setColor(0, 0, 255, 1);
+		
 		GLRenderer.drawQuad(lx, ly - 32, lx + 32, ly + 32);
 		
 	}
